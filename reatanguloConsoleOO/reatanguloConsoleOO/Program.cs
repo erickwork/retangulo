a57713295f9856091f3d340632610d4730668b2f
erickwork <30451415+erickwork@users.noreply.github.com>
2020-03-02 20:47:22 -0300

    Add files via upload

---
+++ reatanguloConsoleOO/reatanguloConsoleOO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RetanguloConsoleOO
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //declarar objeto retangulo
+            Retangulo retangulo1 = new Retangulo();
+
+            //apresentação da solução
+            Console.WriteLine("RETANGULO: calcular perimetro e area");
+            Console.WriteLine();
+
+            //entrada de dados
+            Console.WriteLine("Entre lado A");
+            retangulo1.setLadoA(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Entre lado B");
+            retangulo1.setLadoB(Convert.ToDouble(Console.ReadLine()));
+
+            //processamento
+            retangulo1.calcularPerimetro();
+            retangulo1.calcularArea();
+
+            //saida de dados
+            Console.WriteLine();
+            Console.WriteLine(" === RETANGULO ===");
+            Console.WriteLine("Lado A    =" + retangulo1.getLadoA());
+            Console.WriteLine("Lado B    =" + retangulo1.getLadoB());
+            Console.WriteLine("Perimetro =" + retangulo1.getPerimetro());
+            Console.WriteLine("Area      =" + retangulo1.getArea());
+
+            //para tela, para nao fechar console
+            Console.WriteLine("Tecle<EnTER> para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ reatanguloConsoleOO/reatanguloConsoleOO/Retangulo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetanguloConsoleOO
+{
+    class Retangulo
+    {
+        //declaração das variaveis de instacia / atributos
+        //utilizar o ceonceito OO de ENCAPSULAMENTO
+        private double ladoA;
+        private double ladoB;
+        private double perimetro;
+        private double area;
+
+        //metódo construtor
+        //inicializar as variaveis de instacia com valores efault (regra de negocio)
+        public Retangulo()
+        {
+            this.ladoA = 0;
+            this.ladoB = 0;
+            this.perimetro = 0;
+            this.area = 0;
+
+        }
+        public Retangulo(double ladoA, double ladoB)
+        {
+            this.ladoA = 0;
+            this.ladoB = 0;
+            this.perimetro = 0;
+            this.area = 0;
+
+        }
+
+        //metodos acessores/modoficadores -> Set´´s/get´s
+        public void setLadoA(double ladoA)
+        {
+            this.ladoA = ladoA;
+        }
+        public double getLadoA()
+        {
+            return ladoA;
+        }
+
+
+        public void setLadoB(double ladoB)
+        {
+            this.ladoB = ladoB;
+        }
+        public double getLadoB()
+        {
+            return ladoB;
+        }
+
+        //para as variaveis perimetro e area somente metodos acessores get´s
+        public double getPerimetro()
+        {
+            return perimetro;
+        }
+        public double getArea()
+        {
+            return area;
+        }
+
+        //metodos de processamento
+        public void calcularPerimetro()
+        {
+            perimetro = ladoA * 2 + ladoB * 2;
+        }
+        public void calcularArea()
+        {
+            area = ladoA * ladoB;
+        }
+
+
+     
+    }
+}
